530e58071c3b398e69fccae3c992bdf6ea1f389e
JOchoaArias <ochoaa.jesus16@gmail.com>
2023-06-30 21:02:52 -0300

    PusheoDePruebaDefinitivo

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux'
 import store from './components/Redux/store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
